287d698cecd3d71a1bc5d6340a472fa6776b3b62
Terence Stenvold <tstenvold@gmail.com>
2010-10-05 12:00:24 -0700

    Added a SignalBuilder so that users can build up a request string easily

---
+++ Java/com/socialtext/SignalBuilder.java
@@ -0,0 +1,40 @@
+package com.socialtext;
+
+import java.util.Date;
+
+public class SignalBuilder
+{
+
+    public SignalBuilder(){}
+  
+    public String after(Date after)
+    {
+	return "after=" + (new SimpleDateFormat("yyyy-MM-dd%20HH:mm:ss").format(after)));
+    }
+
+    public String before(Date before)
+    {
+	return "before=" + (new SimpleDateFormat("yyyy-MM-dd%20HH:mm:ss").format(before)));
+    }
+
+    public String follow()
+    {
+	return "follow=1";
+    }
+
+    public String accounts(int id)
+    {
+	return "accounts="+String.parseInt(id);
+    }
+
+    public String orderDesc ()
+    {
+	return "direction=desc";
+    }
+
+    public String orderAsc ()
+    {
+	return "direction=asc";
+    }
+
+}
