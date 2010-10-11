26aeb109be6b7da18dbc82d29a42304d33576627
unknown <stewey2010@hotmail.com>
2010-10-11 15:54:34 -0700

    files updated for simple translate function--- see test.java for an example

--- Java/Test.java
+++ Java/Test.java
@@ -1,14 +1,16 @@
 import com.socialtext.Resting;
 import com.socialtext.Signal;
+import com.google.api.translate.Language;
+import com.google.api.translate.Translate;
 
 public class Test
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         String url = "https://developers.socialtext.net";
         String username = args[0];
         String password = args[1];
-        
+        Translate.setHttpReferrer("localhost");
         Resting r = new Resting(url, username, password);
         
         Signal[] sigs = r.getSignals();
@@ -17,9 +19,13 @@ public class Test
             System.out.println(sigs[i]);
         }
 
+        String translatedText = Translate.execute("Sending a signal from Java... in french"
+                , Language.ENGLISH, Language.FRENCH);
+
         // Testing a post
         Signal dox = new Signal();
-        dox.setBody("Sending a signal from Java...");
+        System.out.println(translatedText);
+        dox.setBody(translatedText);
         r.postSignal(dox);
     }
 }
